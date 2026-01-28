a437649bfc8dc28e5054de918c847b1f96ba972c
Tess Adams <tessadams@New-Wine.local>
2026-01-27 17:53:06 -0700

    shape and triangle

--- app/src/main/java/com/example/bugfix/Shape.java
+++ app/src/main/java/com/example/bugfix/Shape.java
@@ -3,4 +3,6 @@ package com.example.bugfix;
 public abstract class Shape {
     protected int x;
     protected int y;
+
+    String colour = "blue";
 }
--- app/src/main/java/com/example/bugfix/Triangle.java
+++ app/src/main/java/com/example/bugfix/Triangle.java
@@ -1,4 +1,5 @@
 package com.example.bugfix;
 
-public class Triangle {
+public class Triangle extends Shape {
+    public int sides;
 }
